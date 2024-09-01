d68e1352c0016ad09a560c3a98481f50a5e50970
anagonda-ai <alongonda@powerlogin-be2.tau.ac.il>
2024-09-01 13:32:52 +0300

    add unique_dbs.txt file

--- python_scripts/extract_dblinks_for_plantcyc_proteins.py
+++ python_scripts/extract_dblinks_for_plantcyc_proteins.py
@@ -26,26 +26,38 @@ def extract_dblinks(file_path):
 
 def main():
     base_dir = '/groups/itay_mayrose/alongonda/desktop/plantcyc/all_organisms'
-    output_file_path = '/groups/itay_mayrose/alongonda/desktop/plantcyc/all_organisms/dblinks_for_uniqueid.txt'
+    output_file_path = '/groups/itay_mayrose/alongonda/desktop/plantcyc/all_organisms/dblinks_for_uniqueids.txt'
+    unique_dbs_file_path = '/groups/itay_mayrose/alongonda/desktop/plantcyc/all_organisms/unique_dbs.txt'
     
     results = []
+    unique_dbs = set()
 
     # Walk through all directories and files under base_dir
     for root, dirs, files in os.walk(base_dir):
         for file in files:
             if file == 'proteins.dat':
                 file_path = os.path.join(root, file)
-                sub_db = os.path.basename(os.path.dirname(os.path.dirname(os.path.dirname(file_path))))
-                print(f"Processing file: {file_path} for sub_db: {sub_db}")
+                organism = os.path.basename(os.path.dirname(os.path.dirname(os.path.dirname(file_path))))
+                print(f"Processing file: {file_path} for organism: {organism}")
                 matches = extract_dblinks(file_path)
                 if matches:
                     for unique_id, dblinks in matches:
-                        results.append((sub_db, unique_id, dblinks))
+                        results.append((organism, unique_id, dblinks))
+                        # Extract the database name and add to the set of unique DBs
+                        match = re.match(r'\((\w+)', dblinks)
+                        if match:
+                            db_name = match.group(1)
+                            unique_dbs.add(db_name)
 
     # Write the results to the output file
     with open(output_file_path, 'w') as output_file:
-        for sub_db, unique_id, dblinks in results:
-            output_file.write(f'sub_db: {sub_db}, UNIQUE-ID: {unique_id}, DBLINKS: {dblinks}\n')
+        for organism, unique_id, dblinks in results:
+            output_file.write(f'Organism: {organism}, UNIQUE-ID: {unique_id}, DBLINKS: {dblinks}\n')
+
+    # Write the unique database names to the unique_dbs file
+    with open(unique_dbs_file_path, 'w') as unique_dbs_file:
+        for db_name in sorted(unique_dbs):
+            unique_dbs_file.write(f'{db_name}\n')
 
 if __name__ == '__main__':
     main()
