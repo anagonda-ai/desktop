30dfc6d85f1895a5859dae3f2c14adbe24d0b4c4
anagonda-ai <alongonda@powerlogin-be1.tau.ac.il>
2024-09-05 10:45:36 +0300

    extract ENSEMBL db gene sequences

--- python_scripts/extract_dblinks_for_plantcyc_proteins.py
+++ python_scripts/extract_dblinks_for_plantcyc_proteins.py
@@ -9,20 +9,23 @@ def extract_dblinks(file_path):
         matches = []
         unique_id = None
         dblinks_list = []
+        gene = None
         for line in lines:
             if line.startswith('UNIQUE-ID - '):
                 if unique_id and dblinks_list:
                     # Append all collected DBLINKS for the previous UNIQUE-ID
-                    matches.append((unique_id, ' '.join(dblinks_list)))
+                    matches.append((unique_id, gene, ' '.join(dblinks_list)))
                 unique_id = line.split(' - ')[1].strip()
                 dblinks_list = []  # Reset the DBLINKS list for the new UNIQUE-ID
             elif line.startswith('DBLINKS - ') and unique_id:
                 dblinks = line.split(' - ')[1].strip()
                 dblinks_list.append(dblinks)
+            elif line.startswith('GENE - ') and unique_id:
+                gene = line.split(' - ')[1].strip()
 
         # Append the last collected DBLINKS if any
         if unique_id and dblinks_list:
-            matches.append((unique_id, ' '.join(dblinks_list)))
+            matches.append((unique_id, gene, ' '.join(dblinks_list)))
 
         if not matches:
             print(f"No matches found in file: {file_path}")
@@ -35,7 +38,8 @@ def extract_dblinks(file_path):
 def main():
     base_dir = '/groups/itay_mayrose/alongonda/desktop/plantcyc/all_organisms'
     output_file_path = '/groups/itay_mayrose/alongonda/desktop/plantcyc/all_organisms/dblinks_for_uniqueids.txt'
-    unique_dbs_file_path = '/groups/itay_mayrose/alongonda/desktop/plantcyc/all_organisms/unique_dbs.txt'
+    unique_dbs_file_path = '/groups/itay_mayrose/alongonda/desktop/plantcyc/all_organisms/unique_dbs_count.txt'
+    unique_dbs_for_unique_id_file_path = '/groups/itay_mayrose/alongonda/desktop/plantcyc/all_organisms/unique_dbs_for_unique_id.txt'
     
     results = []
     unique_dbs = {}
@@ -49,8 +53,8 @@ def main():
                 print(f"Processing file: {file_path} for organism: {organism}")
                 matches = extract_dblinks(file_path)
                 if matches:
-                    for unique_id, dblinks in matches:
-                        results.append((organism, unique_id, dblinks))
+                    for unique_id, gene, dblinks in matches:
+                        results.append((organism, unique_id, gene, dblinks))
                         # Extract the database name and add to the set of unique DBs
                         db_names = re.findall(r'\((\w+)', dblinks)
                         for db_name in db_names:
@@ -64,20 +68,22 @@ def main():
     
     # Create copy of results to iterate over and remove DBLINKS that are not unique
     results_copy = results.copy()
+    uniques = []
     
     # Iterate through each unique_db in sorted order
     index = 0
     while index < len(sorted_unique_dbs):
         unique_db, _ = sorted_unique_dbs[index]
         new_results = []
-        for organism, unique_id, dblinks in results_copy:
+        for organism, unique_id, gene, dblinks in results_copy:
             db_names = re.findall(r'\((\w+)', dblinks)
             if unique_db in db_names:
                 for db_name in db_names:
                     if db_name != unique_db:
                         unique_dbs[db_name] -= 1
+                uniques.append((organism, unique_id, gene, unique_db))
             else:
-                new_results.append((organism, unique_id, dblinks))
+                new_results.append((organism, unique_id, gene, dblinks))
         results_copy = new_results
 
         # Re-sort unique_dbs after processing each unique_db
@@ -86,13 +92,18 @@ def main():
 
     # Write the results to the output file
     with open(output_file_path, 'w') as output_file:
-        for organism, unique_id, dblinks in results:
-            output_file.write(f'Organism: {organism}, UNIQUE-ID: {unique_id}, DBLINKS: {dblinks}\n')
+        for organism, unique_id, gene, dblinks in results:
+            output_file.write(f'Organism: {organism}, UNIQUE-ID: {unique_id}, GENE: {gene}, DBLINKS: {dblinks}\n')
 
     # Write the unique database names to the unique_dbs file
     with open(unique_dbs_file_path, 'w') as unique_dbs_file:
         for db_name, count in sorted(unique_dbs.items(), key=lambda item: item[1], reverse=True):
             unique_dbs_file.write(f'{db_name}: {count}\n')
+            
+    # Write the results to the output file
+    with open(unique_dbs_for_unique_id_file_path, 'w') as unique_dbs_for_unique_id:
+        for organism, unique_id, gene, unique_db in uniques:
+            unique_dbs_for_unique_id.write(f'Organism: {organism}, UNIQUE-ID: {unique_id}, GENE: {gene}, UNIQUE-DBLINK: {unique_db}\n')
 
 if __name__ == '__main__':
     main()
---
+++ python_scripts/filter_ensembl_by_id.py
@@ -0,0 +1,66 @@
+import os
+import re
+
+def extract_unique_ids(file_path):
+    unique_ids = set()
+    gene_to_id = {}
+    try:
+        with open(file_path, 'r', errors='ignore') as file:
+            lines = file.readlines()
+        for line in lines:
+            if 'ENSEMBL' in line:
+                match = re.search(r'UNIQUE-ID: (\S+)', line)
+                gene = re.search(r'GENE: (\S+)', line)
+                if match:
+                    value = match.group(1).replace('-MONOMER','').replace('MONOMER','').replace(',','')
+                    unique_ids.add(value)
+                    gene_to_id[value] = gene.group(1).replace(',', '')
+    except Exception as e:
+        print(f"Error processing file {file_path}: {e}")
+    
+    return unique_ids, gene_to_id
+
+def search_in_fasta_files(unique_ids, base_dir):
+    results = {}
+    for root, dirs, files in os.walk(base_dir):
+        for file in files:
+            if file.endswith('.canon.fa'):
+                file_path = os.path.join(root, file)
+                try:
+                    with open(file_path, 'r', errors='ignore') as fasta_file:
+                        print(f"Processing file: {file_path}")
+                        found_ids = set()
+                        current_id = None
+                        for line in fasta_file:
+                            if line.startswith('>transcript:'):
+                                current_id = line.split(':')[1].strip()
+                            elif current_id and current_id in unique_ids:
+                                results[current_id] = line.strip() if current_id not in results else f"{results[current_id]}{line.strip()}"
+                                found_ids.add(current_id)
+                        unique_ids -= found_ids  # Remove all found unique_ids at once
+                except Exception as e:
+                    print(f"Error processing file {file_path}: {e}")
+    
+    return results, unique_ids
+
+def main():
+    unique_ids_file_path = '/groups/itay_mayrose/alongonda/desktop/plantcyc/all_organisms/unique_dbs_for_unique_id.txt'
+    fasta_base_dir = '/groups/itay_mayrose/galtoledano/gene_families/data/snakemake_download/per_species'
+    unique_ids_seq = '/groups/itay_mayrose/alongonda/desktop/plantcyc/all_organisms/unique_ids_seq.txt'
+    
+    # Extract UNIQUE-IDs with ENSEMBL UNIQUE-DBLINK
+    unique_ids, gene_to_id = extract_unique_ids(unique_ids_file_path)
+    
+    # Search for UNIQUE-IDs in .canon.fa files
+    results, unfounds = search_in_fasta_files(unique_ids, fasta_base_dir)
+    
+    # Write the results to the output file
+    with open(unique_ids_seq, 'w') as unique_ids_seq_file:
+        for unique_id, value in results.items():
+            unique_ids_seq_file.write(f'{unique_id} (gene {gene_to_id[unique_id]}): {value}\n')
+            
+        for unfound in unfounds:
+            unique_ids_seq_file.write(f'{unfound} (gene {gene_to_id[unique_id]}): N\A\n')
+
+if __name__ == '__main__':
+    main()
