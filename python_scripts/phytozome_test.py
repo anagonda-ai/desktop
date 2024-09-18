947f32aca97b2443512c8d90c1f4ccdc849c6044
anagonda-ai <alongonda@powerlogin-be1.tau.ac.il>
2024-09-18 08:24:42 +0300

    add remove duplicates

--- python_scripts/phytozome_test.py
+++ python_scripts/phytozome_test.py
@@ -23,41 +23,49 @@ def extract_unique_ids(file_path):
 
 def process_unique_id(service, unique_id, gene_to_id):
     try:
-        print(unique_id)
         query = service.new_query("Protein")
         query.add_view("sequence.residues")
         query.add_constraint("primaryIdentifier", "=", unique_id, code="A")
         for row in query.rows():
-            return gene_to_id[unique_id], row["sequence.residues"]
+            gene = gene_to_id[unique_id]
+            sequence = row["sequence.residues"]
+            return gene, sequence
     except Exception as e:
         print(f"Error processing unique_id {unique_id}: {e}")
-        return None
 
-def search_in_phytozome_and_write_to_file(unique_ids, gene_to_id, unique_ids_seq):
+def search_in_phytozome_and_write_to_file(unique_ids, gene_to_id, unique_ids_seq, no_duplicates):
     service = Service("https://phytozome-next.jgi.doe.gov/phytomine/service")
     
     genes_and_sequences = {}
-    with ThreadPoolExecutor(max_workers=16) as executor:
-        futures = {executor.submit(process_unique_id, service, unique_id, gene_to_id): unique_id for unique_id in unique_ids}
-        for future in as_completed(futures):
-            gene, sequence = future.result()
-            if gene:
-                if (gene in genes_and_sequences and len(genes_and_sequences[gene]) < len(sequence)) or gene not in genes_and_sequences:
-                    genes_and_sequences[gene] = sequence
-                        
     with open(unique_ids_seq, 'w') as unique_ids_seq_file:
-        for gene, sequence in genes_and_sequences.items():
-            unique_ids_seq_file.write(f">{gene}\n{sequence}\n")
+        with ThreadPoolExecutor() as executor:
+            futures = {executor.submit(process_unique_id, service, unique_id, gene_to_id): unique_id for unique_id in unique_ids}
+            for future in as_completed(futures):
+                result = future.result()
+                print(f"Result: {result}")
+                if result:
+                    gene, sequence = result
+                    if not no_duplicates:
+                        unique_ids_seq_file.write(f">{gene}\n{sequence}\n")
+                    else:
+                        if gene and ((gene in genes_and_sequences and len(genes_and_sequences[gene]) < len(sequence)) or gene not in genes_and_sequences):
+                                genes_and_sequences[gene] = sequence
+                        
+            if no_duplicates:
+                for gene, sequence in genes_and_sequences.items():
+                    unique_ids_seq_file.write(f">{gene}\n{sequence}\n")
 
 def main():
     unique_ids_file_path = '/groups/itay_mayrose/alongonda/desktop/plantcyc/gene_by_db_seq/unique_dbs_for_unique_id.txt'
-    unique_ids_seq = '/groups/itay_mayrose/alongonda/desktop/plantcyc/gene_by_db_seq/PHYTOZOME/unique_ids_seq.txt'
+    unique_ids_seq = '/groups/itay_mayrose/alongonda/desktop/plantcyc/gene_by_db_seq/PHYTOZOME/unique_ids_seq_test.txt'
     
     # Extract UNIQUE-IDs with ENSEMBL UNIQUE-DBLINK
     unique_ids, gene_to_id = extract_unique_ids(unique_ids_file_path)
     
+    no_duplicates = False
+    
     # Search for UNIQUE-IDs in Phytozome and write to file
-    search_in_phytozome_and_write_to_file(unique_ids, gene_to_id, unique_ids_seq)
+    search_in_phytozome_and_write_to_file(unique_ids, gene_to_id, unique_ids_seq, no_duplicates)
 
 if __name__ == '__main__':
     main()
