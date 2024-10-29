9e110f3d14ea34836f36ddb4d82d87561d2e3a4c
anagonda-ai <alongonda@powerlogin-be1.tau.ac.il>
2024-10-29 09:28:43 +0200

    create df

--- python_scripts/just_scripts/create_df.py
+++ python_scripts/just_scripts/create_df.py
@@ -2,15 +2,17 @@ import os
 import pandas as pd
 
 def create_dataframe(file_path):
-    # Read the file into a DataFrame, skipping the initial comment lines
+    # Read the file into a DataFrame, skipping initial comment lines
     df = pd.read_csv(file_path, sep='\t', comment='#', header=0)
     
-    # Extract the columns that start with 'GENE-NAME-' and 'GENE-ID-'
-    gene_name_cols = [col for col in df.columns if col.startswith('GENE-NAME')]
+    # Drop columns that start with 'GENE-NAME'
+    df = df.loc[:, ~df.columns.str.startswith('GENE-NAME')]
+    
+    # Extract the columns that start with 'GENE-ID-'
     gene_id_cols = [col for col in df.columns if col.startswith('GENE-ID')]
     
     # Rename the columns to match the desired format
-    df.columns = ['UNIQUE-ID', 'NAME'] + gene_name_cols + gene_id_cols
+    df.columns = ['UNIQUE-ID', 'NAME'] + gene_id_cols
     
     return df
 
@@ -27,7 +29,6 @@ def process_pathways_files(base_dir):
     # Concatenate all DataFrames into one
     merged_df = pd.concat(dataframes, ignore_index=True)
     return merged_df
-
 
 def main():
     base_dir = "/groups/itay_mayrose/alongonda/desktop/plantcyc/all_organisms"
