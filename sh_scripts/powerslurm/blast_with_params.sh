1dcea4dbccf9717288efdb4071ddcac0e1544c2b
anagonda-ai <alongonda@mail.tau.ac.il>
2025-05-04 09:56:36 +0300

    add blast_with_params.sh: create a new script for running BLAST with specified parameters and job submission settings

---
+++ sh_scripts/powerslurm/blast_with_params.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=blast_with_params
+#SBATCH --output=/groups/itay_mayrose/alongonda/desktop/example_jobs/out.OU
+#SBATCH --error=/groups/itay_mayrose/alongonda/desktop/example_jobs/error.ER
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=32G
+#SBATCH --partition=itaym
+#SBATCH --time=24:00:00
+
+# Get the input FASTA file from the command line argument
+QUERY_FASTA=$1
+
+# Define the BLAST database
+DB=$2
+
+# Define the output directory
+BLAST_OUTPUT=$3
+
+
+blastp -task blastp-fast -query "$QUERY_FASTA" -db "$DB" -out "$BLAST_OUTPUT" -outfmt 5 -evalue 0.001 -num_threads $(nproc) -max_target_seqs 5
