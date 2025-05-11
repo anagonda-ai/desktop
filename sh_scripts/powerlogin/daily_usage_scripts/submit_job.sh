ee7ae976284599fc328241d0ad78394267ebd6b5
anagonda-ai <alongonda@mail.tau.ac.il>
2025-05-11 09:56:49 +0300

    update job submission scripts: add new scripts for BLAST processing and node login; modify existing scripts for improved functionality and consistency

--- sh_scripts/powerlogin/daily_usage_scripts/submit_job.sh
+++ sh_scripts/powerlogin/daily_usage_scripts/submit_job.sh
@@ -9,8 +9,5 @@
 #PBS -o /groups/itay_mayrose/alongonda/desktop/example_jobs/out.OU
 #PBS -l select=ncpus=32:mem=32gb
 
-conda deactivate
-conda activate plantismash
-
 # Run your script
-python /groups/itay_mayrose/alongonda/tools/plant_mgc_existing_tools/plantismash/run_antismash.py /groups/itay_mayrose/alongonda/datasets/arabidopsis_thaliana/dna/Arabidopsis_thaliana.TAIR10.dna_rm.chromosome.1.fa
+python "/groups/itay_mayrose/alongonda/desktop/python_scripts/move_files.py"
--- sh_scripts/powerslurm/features_flow/blast_organism_on_fasta_and_normalize.sh
+++ sh_scripts/powerslurm/features_flow/blast_organism_on_fasta_and_normalize.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
-#PBS -S /bin/bash
-#PBS -r y
-#PBS -q itaym
-#PBS -v PBS_O_SHELL=bash,PBS_ENVIRONMENT=PBS_BATCH
-#PBS -N sliding_improved
-#PBS -e /groups/itay_mayrose/alongonda/desktop/example_jobs/error.ER
-#PBS -o /groups/itay_mayrose/alongonda/desktop/example_jobs/out.OU
-#PBS -l select=ncpus=32:mem=32gb
+#SBATCH --job-name=blast_organism_on_fasta_and_normalize
+#SBATCH --output=/groups/itay_mayrose/alongonda/desktop/example_jobs/out.OU
+#SBATCH --error=/groups/itay_mayrose/alongonda/desktop/example_jobs/error.ER
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=32G
+#SBATCH --partition=itaym
+#SBATCH --time=24:00:00
 
 # Check if a query file was provided
 if [ "$#" -ne 2 ]; then
@@ -51,8 +50,8 @@ if [ $? -eq 0 ]; then
         # Now the normalized output file is generated with the name 'normalized_blast_scores.csv'
         NORMALIZED_FILE="$OUT_DIR/normalized_blast_scores.csv"
         
-        # Run the filter_normalized_composite_score_for_qid.py script with the normalized results
-        python3 /groups/itay_mayrose/alongonda/desktop/python_scripts/fasta_and_blast/normalize_blast/filter_normalized_composite_score_for_qid.py "$NORMALIZED_FILE"
+        # Run the score_filter.py script with the normalized results
+        python3 /groups/itay_mayrose/alongonda/desktop/python_scripts/bioinformatics/sequence_analysis/blast_utils/score_filter.py "$NORMALIZED_FILE"
 
         # Remove the intermediate files
         rm "$$OUT" "$$CSV_OUT" "$$NORMALIZED_FILE"
